acbdefea55ec7f6d5500a5bb37265870ae0a0031
holidayfun <jhartmann1995@gmail.com>
2016-09-14 14:29:23 +0200

    table manipulation, encapsulation around the provided runtime_cli

---
+++ table_manipulation/runtime_cli.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import argparse
+#import fileinput
+from subprocess import call
+import sys
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="runtime cli")
+    parser.add_argument('-ip', dest='ip', default=None, type=str)
+    parser.add_argument('-port', dest='port', default=10000, type=int)
+    parser.add_argument('-switch', dest='switch', type=int)
+    args = parser.parse_args()
+
+    if args.switch is None and args.ip is None:
+        print('ip oder switch Nummer muss angegeben werden')
+        sys.exit(1)
+    if args.ip is not None:
+        ip = args.ip
+    else:
+        ip = '100.0.0.{0}'.format(100+2*args.switch)
+
+    port = args.port
+    print('Connectiong to {0}:{1}'.format(ip, port))
+    print(call(['/home/hartmann/behavioral-model/tools/runtime_CLI.py',
+                '--thrift-port', str(port),
+                '--thrift-ip', ip],
+                stdin=sys.stdin))
