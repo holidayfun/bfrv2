b591496eabd7bd86469747ea3f82060f27c2895e
holidayfun <jhartmann1995@gmail.com>
2016-09-19 18:14:42 +0200

    16bit length, action renamed

--- table_manipulation/find_posentries.py
+++ table_manipulation/find_posentries.py
@@ -1,8 +1,8 @@
 
-bitstring_len = 8
+bitstring_len = 16
 
 for i in range(0, bitstring_len):
     bitstring = ("{0:0" + str(bitstring_len) + "d}").format(10**i)
     prefix_len = bitstring_len - i
-    output = "table_add find_pos dump_pos 0b{0}/{1} => {2}".format(bitstring, prefix_len, i+1)
+    output = "table_add find_pos save_pos 0b{0}/{1} => {2}".format(bitstring, prefix_len, i+1)
     print(output)
