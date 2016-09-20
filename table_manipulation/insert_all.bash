3f55d668a37b4c7f892cf663677dc9c006abff93
holidayfun <jhartmann1995@gmail.com>
2016-09-20 14:56:28 +0200

    all kinds of table entries

---
+++ table_manipulation/insert_all.bash
@@ -0,0 +1,7 @@
+cli.py -s 1 < bier_ingress
+cli.py -s 1 < find_pos
+cli.py -s 1 < bift_entries_s1
+cli.py -s 1 < check_bfr_id_entries_s1
+cli.py -s 1 < do_clone_recirculation_table
+cli.py -s 1 < do_cloning_table
+cli.py -s 1 < mirroring
