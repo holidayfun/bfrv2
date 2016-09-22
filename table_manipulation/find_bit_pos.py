e833dc1536fddc8824d4a5e722cee033cd9cf2c7
Joshua Hartmann <jhartmann1995@gmail.com>
2016-09-22 15:06:59 +0200

    renamed

--- table_manipulation/find_bit_pos.py
+++ table_manipulation/find_bit_pos.py
@@ -1,8 +1,8 @@
 
-bitstring_len = 16
+bitstring_len = 64
 
 for i in range(0, bitstring_len):
     bitstring = ("{0:0" + str(bitstring_len) + "d}").format(10**i)
     prefix_len = bitstring_len - i
-    output = "table_add find_pos save_pos 0b{0}/{1} => {2}".format(bitstring, prefix_len, i+1)
+    output = "table_add find_bit_pos save_bit_pos 0b{0}/{1} => {2}".format(bitstring, prefix_len, i+1)
     print(output)
