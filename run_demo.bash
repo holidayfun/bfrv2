9e6e39258cebd770c1dc50e8ea9e4e6c06a9ecd7
holidayfun <jhartmann1995@gmail.com>
2016-10-05 14:13:05 +0200

    added p4 compilation before starting demo

--- run_demo.bash
+++ run_demo.bash
@@ -1,6 +1,9 @@
-#clean remains of old mininet sessions
-./reset_mininet.bash
-#copy some data into the mininet folder
-cp RingNetwork.json ../../mininet/RingNetwork.json
-cp build_network.py ../../mininet
-sudo python ../../mininet/build_network.py --behavioral-exe $PWD/bm --json $PWD/bfr.json --pcap-dump test.json
+#recompile bfr
+if p4c-bmv2 p4src/bfr_bier_te.p4 --json bfr.json; then
+    #clean remains of old mininet sessions
+    ./reset_mininet.bash
+    #copy some data into the mininet folder
+    cp RingNetwork.json ../../mininet/RingNetwork.json
+    cp build_network.py ../../mininet
+    sudo python ../../mininet/build_network.py --behavioral-exe $PWD/bm --json $PWD/bfr.json --pcap-dump test.json
+fi
