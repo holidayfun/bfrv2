5f06ddc94edb2534db3f94fbf76ca555d56a76b4
holidayfun <jhartmann1995@gmail.com>
2016-10-05 16:00:34 +0200

    default drop action

--- table_manipulation/find_bit_pos.py
+++ table_manipulation/find_bit_pos.py
@@ -6,3 +6,5 @@ for i in range(0, bitstring_len):
     prefix_len = bitstring_len - i
     output = "table_add find_bit_pos save_bit_pos 0b{0}&&&0b{0} => {1} {2}".format(bitstring, i+1, i+1)
     print(output)
+
+print("table_set_default find_bit_pos _drop")
